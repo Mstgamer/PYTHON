7ab42948321f1ec825373da6c7fb0691b04c4509
mstgamer <ajullalp@gmail.com>
2024-12-19 12:22:13 +0530

    first commit

--- prjct.py
+++ prjct.py
@@ -1,6 +1,11 @@
-i=0
-while i<10:
-    i=i+1
-    if i==5:
-        continue
-    print(i)
+# i=0
+# while i<10:
+#     i=i+1
+#     if i==5:
+#         continue
+#     print(i)
+# num = [1, 2, 3, 4]
+# s = set(num)  
+# print(s)
+emplon = {'name':["Ajul",'manu'], 'age':[21,61], 'salary':["5l","3k"]}
+print(emplon.keys)
